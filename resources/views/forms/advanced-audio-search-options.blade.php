59521f013fe446c628380e29d663bea83440a1c9
user_name <oliverelliotmoon@gmail.com>
2018-07-22 17:02:50 +0100

    advanced options are sent as array

--- resources/views/forms/advanced-audio-search-options.blade.php
+++ resources/views/forms/advanced-audio-search-options.blade.php
@@ -1,14 +1,14 @@
 <div id="advancedsearchform">
   <div class="custom-control custom-checkbox custom-control-inline">
-    <input type="checkbox" id="trackCheckbox" name="trackCheckbox" class="custom-control-input">
-    <label class="custom-control-label" for="trackCheckbox">Track</label>
+    <input type="checkbox" id="track" value="track" name="options[]" class="custom-control-input">
+    <label class="custom-control-label" for="track">Track</label>
   </div>
   <div class="custom-control custom-checkbox custom-control-inline">
-    <input type="checkbox" id="artistCheckbox" name="artistCheckbox" class="custom-control-input">
-    <label class="custom-control-label" for="artistCheckbox">Artist</label>
+    <input type="checkbox" id="artist" value="artist" name="options[]" class="custom-control-input">
+    <label class="custom-control-label" for="artist">Artist</label>
   </div>
   <div class="custom-control custom-checkbox custom-control-inline">
-    <input type="checkbox" id="albumCheckbox" name="albumCheckbox" class="custom-control-input">
-    <label class="custom-control-label" for="albumCheckbox">Album</label>
+    <input type="checkbox" id="album" value="album"  name="options[]" class="custom-control-input">
+    <label class="custom-control-label" for="album">Album</label>
   </div>
 </div>
