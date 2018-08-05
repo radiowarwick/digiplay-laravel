29b739e5dcd78fc8c5084f8687c863b1a4934507
user_name <oliverelliotmoon@gmail.com>
2018-08-05 17:59:16 +0100

    changed option name from track to title to match with search function

--- resources/views/forms/advanced-audio-search-options.blade.php
+++ resources/views/forms/advanced-audio-search-options.blade.php
@@ -1,7 +1,7 @@
 <div id="advancedsearchform">
   <div class="custom-control custom-checkbox custom-control-inline">
-    <input type="checkbox" id="track" value="track" name="options[]" class="custom-control-input">
-    <label class="custom-control-label" for="track">Track</label>
+    <input type="checkbox" id="title" value="title" name="options[]" class="custom-control-input">
+    <label class="custom-control-label" for="title">Track</label>
   </div>
   <div class="custom-control custom-checkbox custom-control-inline">
     <input type="checkbox" id="artist" value="artist" name="options[]" class="custom-control-input">
