536597a827961259cbb24e3e640064b4ec03aba6
user_name <oliverelliotmoon@gmail.com>
2018-08-05 20:01:13 +0100

    checks array is not empty before searching for presence of options

--- resources/views/forms/advanced-audio-search-options.blade.php
+++ resources/views/forms/advanced-audio-search-options.blade.php
@@ -1,14 +1,14 @@
 <div id="advancedsearchform">
   <div class="custom-control custom-checkbox custom-control-inline">
-    <input type="checkbox" id="title" value="title" name="options[]" class="custom-control-input" @if(in_array("title",$options)) {{"checked"}} @endif>
+    <input type="checkbox" id="title" value="title" name="options[]" class="custom-control-input" @if(!empty($options) && in_array("title",$options)) {{"checked"}} @endif>
     <label class="custom-control-label" for="title">Track</label>
   </div>
   <div class="custom-control custom-checkbox custom-control-inline">
-    <input type="checkbox" id="artist" value="artist" name="options[]" class="custom-control-input" @if(in_array("artist",$options)) {{"checked"}} @endif>
+    <input type="checkbox" id="artist" value="artist" name="options[]" class="custom-control-input" @if(!empty($options) && in_array("artist",$options)) {{"checked"}} @endif>
     <label class="custom-control-label" for="artist">Artist</label>
   </div>
   <div class="custom-control custom-checkbox custom-control-inline">
-    <input type="checkbox" id="album" value="album"  name="options[]" class="custom-control-input" @if(in_array("album",$options)) {{"checked"}} @endif>
+    <input type="checkbox" id="album" value="album"  name="options[]" class="custom-control-input" @if(!empty($options) && in_array("album",$options)) {{"checked"}} @endif>
     <label class="custom-control-label" for="album">Album</label>
   </div>
 </div>
