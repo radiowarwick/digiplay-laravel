f688a75bdd414f5bf090311e364fb6fe6bc35e06
user_name <oliverelliotmoon@gmail.com>
2018-08-07 18:45:17 +0100

    got rid of the bootstrap styling on checkboxes that does not fit the colour scheme

--- resources/views/forms/advanced-audio-search-options.blade.php
+++ resources/views/forms/advanced-audio-search-options.blade.php
@@ -2,37 +2,37 @@
 
   <div class="grid">
     <div class="row">
-      <div class="custom-control custom-checkbox custom-control-inline">
-        <input type="checkbox" id="title" value="title" name="options[]" class="custom-control-input" {{ (empty($options) or in_array("title",$options)) ? "checked" : "" }}>
-        <label class="custom-control-label" for="title">Title</label>
+      <div class="custom-control-inline">
+        <input type="checkbox" id="title" value="title" name="options[]"  {{ (empty($options) or in_array("title",$options)) ? "checked" : "" }}>
+         Title
       </div>
-      <div class="custom-control custom-checkbox custom-control-inline">
-        <input type="checkbox" id="artist" value="artist" name="options[]" class="custom-control-input" {{ (empty($options) or in_array("artist",$options)) ? "checked" : "" }}>
-        <label class="custom-control-label" for="artist">Artist</label>
+      <div class="custom-control-inline">
+        <input type="checkbox" id="artist" value="artist" name="options[]"  {{ (empty($options) or in_array("artist",$options)) ? "checked" : "" }}>
+        Artist
       </div>
-      <div class="custom-control custom-checkbox custom-control-inline">
-        <input type="checkbox" id="album" value="album"  name="options[]" class="custom-control-input" {{ (empty($options) or in_array("album",$options)) ? "checked" : "" }}>
-        <label class="custom-control-label" for="album">Album</label>
+      <div class="custom-control-inline">
+        <input type="checkbox" id="album" value="album"  name="options[]"  {{ (empty($options) or in_array("album",$options)) ? "checked" : "" }}>
+        Album
       </div>
     </div>
 
   <div class="row">
-      <div class="custom-control custom-checkbox custom-control-inline" id="types">
-        <input type="checkbox" id="Music" value="Music" name="types[]" class="custom-control-input" {{ (empty($types) or in_array("Music",$types)) ? "checked" : "" }}>
-        <label class="custom-control-label" for="Music">Music</label>
+      <div class="custom-control-inline" id="types">
+        <input type="checkbox" id="Music" value="Music" name="types[]"  {{ (empty($types) or in_array("Music",$types)) ? "checked" : "" }}>
+        Music
       </div>
-      <div class="custom-control custom-checkbox custom-control-inline" id="types">
-        <input type="checkbox" id="Jingle" value="Jingle" name="types[]" class="custom-control-input" {{ (!empty($types) and in_array("Jingle",$types)) ? "checked" : "" }}>
-        <label class="custom-control-label" for="Jingle">Jingle</label>
+      <div class="custom-control-inline" id="types">
+        <input type="checkbox" id="Jingle" value="Jingle" name="types[]"  {{ (!empty($types) and in_array("Jingle",$types)) ? "checked" : "" }}>
+        Jingle
       </div>
-      <div class="custom-control custom-checkbox custom-control-inline" id="types">
-        <input type="checkbox" id="Advert" value="Advert" name="types[]" class="custom-control-input" {{ (!empty($types) and in_array("Advert",$types)) ? "checked" : "" }}>
-        <label class="custom-control-label" for="Advert">Advert</label>
+      <div class="custom-control-inline" id="types">
+        <input type="checkbox" id="Advert" value="Advert" name="types[]"  {{ (!empty($types) and in_array("Advert",$types)) ? "checked" : "" }}>
+        Advert
       </div>
       @if(auth()->user()->hasPermission('Can schedule prerecs'))
-        <div class="custom-control custom-checkbox custom-control-inline" id="types">
-          <input type="checkbox" id="Prerec" value="Prerec" name="types[]" class="custom-control-input"  {{ (!empty($types) and in_array("Prerec",$types)) ? "checked" : "" }}>
-          <label class="custom-control-label" for="Prerec">Prerec</label>
+        <div class="custom-control-inline" id="types">
+          <input type="checkbox" id="Prerec" value="Prerec" name="types[]"   {{ (!empty($types) and in_array("Prerec",$types)) ? "checked" : "" }}>
+          Prerec
         </div>
       @endif
     </div>
