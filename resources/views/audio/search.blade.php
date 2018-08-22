1c542f5239dff1b01a09519a21fabf92cb66ca65
user_name <oliverelliotmoon@gmail.com>
2018-08-22 19:34:24 +0100

    search results formatted into table

--- resources/views/audio/search.blade.php
+++ resources/views/audio/search.blade.php
@@ -12,12 +12,26 @@
 	@include('forms.audio-search')
 
 	<h3>You got {{ $total }} results</h3>
-
-	<ul>
-		@foreach($results as $r)
-			<li>{{ $r->title }} by {{ $r->artist->name }}</li>
-		@endforeach
-	</ul>
+  
+  @if($total>0)
+    <table class="table table-striped" cellspacing="0">
+      <thead>
+	  		<th class="title">Title</th>
+	  		<th class="artist">Artist</th>
+	  		<th class="album">Album</th>
+	  		<th class="length">Length</th>
+        @foreach($results as $r)
+          <tr>
+            <td class="title"> {{ $r->title }} </td>
+            <td class="artist"> {{ $r->artist->name }} </td>
+            <td class="album"> {{ $r->album->name }} </td>
+            <td class="length">  2m </td>
+           </td>
+          </tr>
+        @endforeach
+      </thead>
+    </table>
+  @endif
 
 	{{ $results->appends(['q' => $q])->links() }}
 @endsection
