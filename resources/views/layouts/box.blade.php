9474a1cbb8c3367ffd2876c8a1b0f0857fe6e4bf
Connor <c.powell924@gmail.com>
2018-10-23 22:38:51 +0100

    added theme-colour

--- resources/views/layouts/box.blade.php
+++ resources/views/layouts/box.blade.php
@@ -6,6 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">	
 		<meta charset="utf-8">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<meta name="theme-color" content="#2b2b2b">
 
 		<link rel="stylesheet" type="text/css" href="/css/app.css">
 		<link rel="shortcut icon" href="/img/favicon.ico">
